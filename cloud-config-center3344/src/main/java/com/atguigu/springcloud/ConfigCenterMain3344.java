bb82e23335bb3e2981033f369c99e6da704a903a
lxeey <sevenxylee@163.com>
2022-09-08 16:19:27 +0800

    Spring config配置中心

---
+++ cloud-config-center3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lixinyi03
+ * @version 1.0
+ * @date 2022/9/1 15:57
+ * @Description
+ */
+@SpringBootApplication
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
