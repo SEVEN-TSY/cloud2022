be68e318a8352205e6ec8ca3cf40eb4db6a8297e
lxeey <sevenxylee@163.com>
2022-09-12 22:17:37 +0800

    Spring config配置中心+Bus动态刷新

--- cloud-config-center3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author lixinyi03
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description
  */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344.class,args);
